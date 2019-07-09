83547046b469397dfc6e9c8ca47b1923a5056c11
Eduardo A. Paris Penas <edward2a@gmail.com>
2019-07-09 16:40:38 +0100

    initial setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name            = 'pyxmldoc',
+    version         = '0.0.1',
+    description     = 'XML avec un peu de bon sens (XML with a little common sense)',
+    url             = 'https://github.com/edward2a/pyxmldoc',
+    author          = 'Eduardo A. Paris Penas - Real ReadMe Limited',
+    author_email    = 'edward2a@gmail.com, epp@realread.me',
+    license         = 'Apache License 2.0',
+    packages        = ['pyxmldoc'],
+    zip_safe        = True
+    )
+
