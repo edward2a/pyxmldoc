717a0dd1816192b8f917056c3d4e65a682bed7a9
Eduardo A. Paris Penas <edward2a@gmail.com>
2019-07-09 16:22:25 +0100

    initial work

---
+++ pyxmldoc.py
@@ -0,0 +1,27 @@
+from xml.etree import ElementTree as et
+
+
+class XmlDocument(object):
+
+    def __init__(self):
+        pass
+
+    def load_file(self, f):
+        self.source_data = et.parse(f)
+
+    def xml_to_dict(self):
+        xmldoc = {}
+
+        # Get root
+        root = self.souce_data.getroot()
+        xmldoc = self.load_tree(root)
+
+    @staticmethod
+    def load_element(e):
+        return {'attrs': e.attrib, 'children': [
+            {c.tag: {'attrs': c.attrib}} for c in e.getchildren() ]}
+            
+
+    def load_tree(self, e):
+        return {e.tag: {'attrs': e.attrib, 'children': [
+            self.load_tree(c) for c in e.getchildren() ]}}
